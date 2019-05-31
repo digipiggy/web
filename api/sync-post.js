e8d1775cbcaa857f4e77803541b74eb2e1d3c2be
Tony Gemoll <anthonygemoll@northwesternmutual.com>
2019-05-31 10:58:06 -0500

    testing sync function

---
+++ api/sync-post.js
@@ -0,0 +1,21 @@
+module.exports = async (req, res) => {
+    try {
+        shared.cors.setStandardHeaders(res);
+
+        if (req.method === 'OPTIONS') {
+            res.statusCode = 204;
+            res.setHeader('Content-Length', '0');
+            return res.end();
+        } else if (req.method === 'PUT') {
+            let payload = {
+                temp: process.env.PIGGY_API_KEY
+            };
+
+            return res.end(JSON.stringify(payload));
+        }
+    } catch (err) {
+        console.error(err);
+        res.statusCode = 500;
+        return res.end('Unexpected Error');
+    }
+};
