4e28d7dcf6a5c784f98a6b0fc8eb82853b297470
Scott Butler <scottbutler@northwesternmutual.com>
2021-03-08 15:55:50 -0600

    saving minor changes

--- app/src/components/ArticleCard.vue
+++ app/src/components/ArticleCard.vue
@@ -7,7 +7,7 @@
       class="mb-4"
     ></v-img>
     <p class="text-caption">Published 1/19/21 </p>
-    <p>We've all been there: your in the toy isle at target, and the latest paw patrol plushy catches your daughters eye. Unfortunatley for her, she's... </p>
+    <p>We've all been there: you're in the toy isle at target, and the latest Paw Patrol plushy catches your daughters eye. Unfortunatley for her, she's... </p>
     <v-btn color="#48A182" dark to="/lessons/lesson/1">
       Continue Reading
     </v-btn>
