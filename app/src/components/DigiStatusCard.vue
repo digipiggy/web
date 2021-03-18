50139b79c2f6ee6dffb1317e2524df6a7389d302
Scott Butler <scottbutler@northwesternmutual.com>
2021-03-18 00:18:56 -0500

    updated the status card and goal cards for both logic and appearance

--- app/src/components/DigiStatusCard.vue
+++ app/src/components/DigiStatusCard.vue
@@ -1,61 +1,35 @@
 <template>
   <v-card class="elevation-3 pa-5">
     <p class="text-h6" >My Digi Pig - {{device.deviceCode}} </p>
-    <!-- <v-sheet class="mb-4 px-14">
-      <v-img 
-        :src="require('@/assets/logoLightGreen.png')" 
-        aspect-ratio="1.4"
-
-      ></v-img>
-    </v-sheet> -->
-     <!-- <v-list-item
-        v-for="(g, i) in enabledGoals"
-        :key="`${g.name}-${i}`"
-      >
-      <v-list-item-avatar>
-        <v-progress-circular
-          class="align-center"
-          :rotate="270"
-          :width="15"
-          :value="getCirclePercent(g.current, g.total)"
-          :color="$color(g.color)"
-        ></v-progress-circular>
-      </v-list-item-avatar>
-
-      <v-list-item-content>
-        <v-list-item-title v-text="g.name"></v-list-item-title>
-
-        <v-list-item-subtitle >{{g.current}}/{{g.total}}</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item> -->
-
     <v-row 
-      v-for="kid in device.kids"
-      :key="`goal-${kid}`"
+      v-for="(goal, i) in device.goals"
+      :key="`StatusGoal-${i}`"
       class="mb-2"
     >
-      <v-col v-if="device.goals[kid.goalIndex].name == 'Select A New Goal'">
-        <p class="mb-2">{{kid.name}}'s Goal has not been selected from the Goal Catalog.</p>
+      <v-col v-if="!goal.enabled">
+        <p class="mb-2">{{device.kids[i].name}}'s Goal has not been selected yet, please update this in lesson 1.</p>
       </v-col>
       <v-col v-else>
-        <p>{{kid.name}}'s Goal</p>
+        <p>{{device.kids[i].name}}'s Goal</p>
         <v-row class="mb-2">
           <v-img 
-            v-for="coinDot in 3"
+            v-for="coinDot in goal.current"
             :key="`coinDot-${coinDot}`"
             :src="require('@/assets/PigglesCoin.png')" 
             aspect-ratio="1"
+            max-width="12%"
           ></v-img>
           <v-img 
-            v-for="blankLED in 5"
+            v-for="blankLED in goal.total - goal.current"
             :key="`blankLED-${blankLED}`"
             :src="require('@/assets/BlankLED.png')" 
             aspect-ratio="1"
+            max-width="12%"
           ></v-img>
         </v-row>
       </v-col>
     </v-row>
-    <v-btn color="#9367E6" dark to="/pigSettings">
+    <v-btn color="#9367E6" dark to="/goals">
       Update My Goals
     </v-btn>
   </v-card>
--- app/src/views/Goals.vue
+++ app/src/views/Goals.vue
@@ -1,46 +1,10 @@
 <template>
   <v-container fluid >
-    <!-- <v-layout align-center justify-center>
-      <v-flex xs12 sm8 md4>
-        <div>
-          <v-card class="elevation-12 mb-4">
-            <v-card-text class="grey lighten-5">
-              <v-alert
-                class="mb-3"
-                :value="!totalPercentageValid"
-                type="warning"
-              >Total percentage does not add up to 100.</v-alert>
-              <v-expansion-panels popout>
-                  <v-expansion-panel v-for="(goal, i) in goals" :key="i">
-                    <v-expansion-panel-header >
-                      <v-chip color="white" :class="(goal.enabled ? 'goal-enabled' : '')">
-                        <v-avatar :color="goal.enabled ? $color(goal.color) : 'grey'"></v-avatar>
-                        <span class="ml-2">{{goal.kidsName}}' Goal</span>
-                      </v-chip>
-                    </v-expansion-panel-header>
-                    <v-expansion-panel-content>
-                      <goal v-model="goals[i]" :busy="busy" @valid="onValid(i, $event)"/>
-                    </v-expansion-panel-content>
-                  </v-expansion-panel>
-              </v-expansion-panels>
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                color="primary"
-                @click="onSave"
-                :disabled="!totalPercentageValid || !allGoalsValid || busy"
-              >Save Goals
-                <v-icon right dark>cloud_upload</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </div>
-      </v-flex>
-    </v-layout> -->
+
     <v-container v-if="!device.status.completedPreferences">
       <halt actionTodo="access your goals"/>
     </v-container>
+
     <v-container v-else-if="device.goals.length == 0">
       <v-row>
         <v-col cols="12">
@@ -58,6 +22,7 @@
         </v-col>
       </v-row>
     </v-container>
+
     <v-container v-else>
       <v-row>
         <v-col cols="12">
@@ -95,8 +60,6 @@
           md="6"
         >
           <v-card class="pa-5 rounded-sm" height="100%" :style="{borderTop: `5px solid ${translateColor(goal.color)}`}">
-            <!-- <v-avatar rounded="circle" :color="goal.enabled ? $color(goal.color) : 'grey'" size="36"></v-avatar> -->
-            <!-- <span> {{goal.kidsName}}'s Goal</span> -->
             <v-row justify="space-between">
               <v-col>
                 <p class="text-h5 font-weight-medium " style="color: #48A182">{{goal.kidsName}}</p>
@@ -111,7 +74,6 @@
                     mdi-checkbox-blank-circle-outline
                   </v-icon>
                 </p>
-                 
               </v-col>
             </v-row>
             <p class="text-caption font-weight-light mb-0" style="color: #2C2C2C">{{goal.kidsName}}'s Goal</p>
@@ -126,29 +88,27 @@
               <v-row>
                 <v-col cols="6">
                   <p class="text-body-1 text-center font-weight-light" style="color: #2C2C2C">Current Amount</p>
-                  <v-sheet class="pl-8">
+                  <v-sheet class="d-flex justify-center align-center">
                     <v-btn
-                      class="mx-2"
-                      style="display: inline;"
+                      class="mr-3"
                       fab
                       dark
                       small
                       color="#9367E6"
-                      @click="goal.current -= 1"
+                      @click="updateGoalCurrentAmount(i, -1)"
                     >
                       <v-icon dark>
                         mdi-minus
                       </v-icon>
                     </v-btn>
-                    <p class="text-h1 font-weight-bold text-center" style="color: #48A182; display: inline;">{{goal.current}}</p>
+                    <p class="text-h1 font-weight-bold mb-0" style="color: #48A182;">{{goal.current}}</p>
                     <v-btn
-                      class="mx-2"
-                      style="display: inline;"
+                      class="ml-3"
                       fab
                       dark
                       small
                       color="#9367E6"
-                      @click="goal.current += 1"
+                      @click="updateGoalCurrentAmount(i, 1)"
                     >
                       <v-icon dark>
                         mdi-plus
@@ -161,18 +121,20 @@
                   <p class="text-h1 font-weight-bold text-center" style="color: #48A182">{{goal.total}}</p>
                 </v-col>
               </v-row>
-              <v-row class="mb-7 px-10">
+              <v-row class="mb-4 px-2 justify-center">
                 <v-img 
                   v-for="coinDot in goal.current"
                   :key="`coinDot-${coinDot}`"
                   :src="require('@/assets/PigglesCoin.png')" 
                   aspect-ratio="1"
+                  max-width="12%"
                 ></v-img>
                 <v-img 
                   v-for="blankLED in (goal.total - goal.current)"
                   :key="`blankLED-${blankLED}`"
                   :src="require('@/assets/BlankLED.png')" 
                   aspect-ratio="1"
+                  max-width="12%"
                 ></v-img>
               </v-row>
             </v-container>
@@ -265,6 +227,14 @@ export default {
         this.totalPercentageValid = total === 100;
       }
     },
+    updateGoalCurrentAmount(goalIndex, amount) {
+      const goal = this.goals[goalIndex];
+      const newCurrentAmount = goal.current + amount;
+      if (newCurrentAmount >= 0 && newCurrentAmount <= goal.total) {
+        goal.current += amount
+      }
+      return
+    },
     convertColor(color){
       let hex = converter.decToHex(color.toString());
       return hex.slice(2, hex.length);
@@ -333,9 +303,7 @@ export default {
     async onSave() {
       this.busy = true;
       const device = {
-        deviceId: this.device.deviceId,
-        deviceCode: this.device.deviceCode,
-        piggySleep: this.device.piggySleep,
+        ...this.device,
         goals: this.goals.map(g => {
           return {
             name: g.name,
@@ -349,6 +317,8 @@ export default {
           };
         })
       };
+
+      console.log("device", device)
 
       if (await this.updateDevice(device)) {
         this.displayMessage({ text: 'Goals updated', color: 'info' });
