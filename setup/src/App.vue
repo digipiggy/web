d1ca83647f702f3acab0c113381df2c18ef8ac5f
Scott Butler <scottbutler@northwesternmutual.com>
2021-03-21 09:51:40 -0500

    update to the setup repo

--- setup/src/App.vue
+++ setup/src/App.vue
@@ -10,14 +10,51 @@
               <v-toolbar dark color="#9367E6">
                 <v-toolbar-title>WiFi Setup</v-toolbar-title>
               </v-toolbar>
-              <v-layout align-center justify-center row v-show="busy">
-                <p>{{connectionStepText}}</p>
-              </v-layout>
-              <v-layout align-center justify-center row v-show="busy">
-                <loader color="#1976d2" class="my-3"/>
-              </v-layout>
+              <v-container >
+                <v-row                     
+                  v-for="(step, i) in connectionSteps"
+                  :key="`step-${i}`"
+                  class="mb-3" justify="center">
+                  <p class=text-center>
+                    {{step.text}}
+                    <v-progress-circular
+                      v-if="step.status == 'in progress'"
+                      size="18"
+                      indeterminate
+                      color="purple"
+                    ></v-progress-circular>
+                    <v-icon 
+                      v-if="step.status == 'success'"
+                      dark 
+                      size="18"
+                      color="green"
+                    >
+                      check
+                    </v-icon>
+                    <v-icon 
+                      v-if="step.status == 'failed'"
+                      size="18"
+                      dark 
+                      color="red"
+                    >
+                      close
+                    </v-icon>
+                  </p>
+                </v-row>
+                <v-row class="mx-3">
+                  <p v-if="failedSetup">
+                    Oh no, something went wrong. Please try again. Double check that you have entered your WiFi password correctly. If trouble persists, please reach out to our team for assistance. 
+                  </p>
+                </v-row>
+                
+              </v-container>
+
               <v-card-text>
-                <p>
+                <p v-if="failedSetup">
+                  Wait until the DigiPig re-enters WiFi pairing mode (4 blue bars), re-connect to the network that begins with
+                  <span class="font-weight-black">DIGIPIGGY</span>, and then click refresh.
+                </p>
+                <p v-else>
                   Connect to the network that begins with
                   <span class="font-weight-black">DIGIPIGGY</span> and then click continue.
                 </p>
@@ -52,13 +89,13 @@
                   v-model="password"
                   :disabled="busy"
                 ></v-text-field>
-                <v-btn color="primary" @click="onContinue" :disabled="busy || !claimCode">
+                <v-btn class="white--text" color="#9367E6" @click="onContinue" :disabled="busy || !claimCode">
                   {{ continueButtonText }}
                   <v-icon v-show="networks.length" right dark>refresh</v-icon>
                 </v-btn>
                 <v-btn
-                  class="ml-2"
-                  color="primary"
+                  class="ml-2 white--text"
+                  color="#9367E6"
                   @click="onConnect"
                   :disabled="busy"
                   v-show="networks.length"
@@ -104,7 +141,8 @@ export default {
       notificationText: '',
       notificationColor: 'info',
       checkingWifiConfig: false,
-      connectionStepText: ''
+      failedSetup: false,
+      connectionSteps: []
     };
   },
   computed: {
@@ -143,7 +181,13 @@ export default {
       const network = this.networks[this.selectedNetwork];
       try {
         this.busy = true;
-        this.connectionStepText = 'Configuring device'
+        // this.connectionStepText = 'Configuring device'
+        this.connectionSteps = [];
+        this.failedSetup = false;
+        this.connectionSteps.push({
+          text: 'Configuring DigiPig device',
+          status: 'in progress'
+        });
         this.deviceID = await sap.deviceInfo().then(res => res.id);
         if (this.claimCode !== "wifireset") {
           await sap.setClaimCode(this.claimCode);
@@ -161,31 +205,57 @@ export default {
         // Then the user's device (laptop, cell phone, etc) will automatically re-connect to their wifi network
         // Wait until they are back on their network before proceeding.
         
-        this.connectionStepText = 'Reconnecting to your wifi.'
+        this.connectionSteps[0].status = 'success'
+        this.connectionSteps.push({
+          text: 'Automatically reconnecting to your WiFi',
+          status: 'in progress'
+        });
+        // this.connectionStepText = 'Automatically reconnecting to your WiFi'
         let wiFiOnline = await this.checkIfWiFiIsOnline();
-        if (!wiFiOnline) return;
-        
-        let deviceOnline = false;
-        this.connectionStepText = 'Confirming Digi-Pig connected to network.'
+        if (!wiFiOnline) {
+          this.connectionSteps[1].status = 'failed';
+          this.failedSetup = true;
+          return;
+        }
+        this.connectionSteps[1].status = 'success';
 
+        let deviceOnline = false;
+        // this.connectionStepText = 'Confirming DigiPig is online.'
+
+        this.connectionSteps.push({
+          text: 'Confirming DigiPig is online',
+          status: 'in progress'
+        });
         // TODO: remove access token from URI
         // TODO: move this to it's own function
         await axios.put(`https://api.particle.io/v1/products/8466/devices/${this.deviceID}/ping?access_token=5043aee54ba12b3d3968325d3e653fc6eaf1e693`)
         .then(response => {
           if (response.data.online) {
             deviceOnline = true; 
+            this.connectionSteps[2].status = 'success';
           }
           else {
+            this.connectionSteps[2].status = 'failed';
+            this.failedSetup = true;
             this.handleError(response, 'Pig failed to connect to WiFi network. Please try again.')
           }
         }).catch(err => {
+          this.connectionSteps[2].status = 'failed';
+          this.failedSetup = true;
           this.handleError(err, 'Failed to ping the pig. Please try again.')
         });
 
         if (!deviceOnline) return;
 
+        this.connectionSteps[2].status = 'success';
+
+        this.connectionSteps.push({
+          text: 'Connection successful! You will automatically be returned to the setup page',
+          status: 'success'
+        });
+
         await new Promise(resolve => {
-          this.connectionStepText = 'Connection successful. Time to get Digi.'
+          // this.connectionStepText = 'Connection successful! You will automatically be returned to the setup page. '
           setTimeout(() => {
             resolve()
           }, 5000)
@@ -198,10 +268,10 @@ export default {
           window.location = `${process.env.VUE_APP_WIFI_REDIRECT_URL}/pigSettings`;
         }
       } catch (err) {
+        this.failedSetup = true;
         this.handleError(err, 'Failed to connect to WiFi network');
       } finally {
         this.busy = false;
-        this.connectionStepText = ''
       }
     },
     async checkIfWiFiIsOnline() {
